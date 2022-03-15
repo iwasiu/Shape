177e30b1620042f6022518761a6c8b42183a5954
idwou <iwasiu460@gmail.com>
2022-03-15 12:39:45 +0100

    1. created some attributes of any quadrilaterale and provided their getters and setters 2. implemented two abstract methods-method to find perimeter and method to find area of a plane shape

--- src/com/teeta90array/Shape/Quadrilaterals/Quadrilaterals.java
+++ src/com/teeta90array/Shape/Quadrilaterals/Quadrilaterals.java
@@ -5,20 +5,41 @@ import com.teeta90array.Shape.planeShape;
 public class Quadrilaterals extends planeShape{
 	private double length, breadth;
 	
-
+//	constructor
 public Quadrilaterals(double length, double breadth) {
 		super();
 		this.length = length;
 		this.breadth = breadth;
 	}
 
-//	implementation of abstract method to find perimeter of a shape
+//	getters and setters methods
+public double getLength() {
+	return length;
+}
+
+
+public void setLength(double length) {
+	this.length = length;
+}
+
+
+public double getBreadth() {
+	return breadth;
+}
+
+
+public void setBreadth(double breadth) {
+	this.breadth = breadth;
+}
+
+
+	//	implementation of abstract method to find perimeter of a shape
 	public void getPerimeter() {
-	System.out.println("perimeter method");	
+	System.out.println("perimeter method for Quadrilateral");	
 	}
 
 //	implementation of abstract method to find area of a shape
 	public void getArea() {
-		System.out.println("area method");	
+		System.out.println("area method for Quadrilateral");	
 	}
 }
