54a26fe8b86330c7d9c78709d922739ef78920cf
idwou <iwasiu460@gmail.com>
2022-03-17 08:20:14 +0100

    modified the super class fields and cleaned up their getter and setter methods

--- src/com/teeta90array/Shape/planeShape.java
+++ src/com/teeta90array/Shape/planeShape.java
@@ -6,33 +6,9 @@ public abstract class planeShape {
 	private static int count = 0;
 	
 	//	attributes for shape
-	protected double radius, diameter,arclength,length, breadth, height, base;
+	protected double length, breadth;
 	
 	//	getter and setter methods
-	public double getRadius() {
-		return radius;
-	}
-
-	public void setRadius(double radius) {
-		this.radius = radius;
-	}
-
-	public double getDiameter() {
-		return diameter;
-	}
-
-	public void setDiameter(double diameter) {
-		this.diameter = diameter;
-	}
-
-	public double getArclength() {
-		return arclength;
-	}
-
-	public void setArclength(double arclength) {
-		this.arclength = arclength;
-	}
-
 	public double getLength() {
 		return length;
 	}
@@ -48,27 +24,10 @@ public abstract class planeShape {
 	public void setBreadth(double breadth) {
 		this.breadth = breadth;
 	}
-
-	public double getHeight() {
-		return height;
-	}
-
-	public void setHeight(double height) {
-		this.height = height;
-	}
-
-	public double getBase() {
-		return base;
-	}
-
-	public void setBase(double base) {
-		this.base = base;
-	}
-
+	
 	//	a method to find perimeter of a shape
 	public abstract void getPerimeter();
 
 	//	a method to find area of a shape
 	public abstract void getArea();
-	
 }
