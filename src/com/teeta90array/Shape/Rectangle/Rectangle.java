93aff616ae7b3086a9e7012de679e0d2dd485b61
idwou <iwasiu460@gmail.com>
2022-03-23 09:29:48 +0100

    created no-arg constructor and one arg constructor for rectangle class.Also provided the getter setter methods for the attribute 'length'.And added two methods to find its perimeter and area

--- src/com/teeta90array/Shape/Rectangle/Rectangle.java
+++ src/com/teeta90array/Shape/Rectangle/Rectangle.java
@@ -1,5 +1,48 @@
-package com.teeta90array.Shape.Polygons;
+package com.teeta90array.Shape.Rectangle;
 
-public class Polygon {
+public class Rectangle {
+    private double length, breadth;
 
+    public Rectangle() {
+    }
+
+    public Rectangle(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getBreadth() {
+        return breadth;
+    }
+
+    public void setBreadth(double breadth) {
+        this.breadth = breadth;
+    }
+        //	A method to find perimeter of a square
+	public void getPerimeter() {
+		System.out.println("The perimeter of a rectangle(: "+this.length+" ,"+this.breadth+")"
+                                    + " = " + perimeter());
+	}
+
+    private double perimeter() {
+       return 2 * (this.length+this.breadth);
+    }
+
+	//	A method to find area of a square
+	public void getArea() {
+		System.out.println("The area of a rectangle(: "+this.length+" ,"+this.breadth+")"
+                                    + " = " +(area()));	
+	}
+
+    private double area() {
+        return this.length * this.breadth;
+    }
 }
