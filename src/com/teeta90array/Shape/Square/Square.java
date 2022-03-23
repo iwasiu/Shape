8d1a7af0ac4873b48a3f9056635508bb731a9fab
idwou <iwasiu460@gmail.com>
2022-03-23 09:12:50 +0100

    created no-arg constructor and one arg constructor for square class.Also provided the getter setter methods for the attribute 'length'.And added two methods to find its perimeter and area

--- src/com/teeta90array/Shape/Square/Square.java
+++ src/com/teeta90array/Shape/Square/Square.java
@@ -1,5 +1,44 @@
-package com.teeta90array.Shape.Polygons;
+package com.teeta90array.Shape.Square;
 
-public class Polygon {
+import com.teeta90array.Shape.Quadrilaterals.Quadrilaterals;
+
+public class Square extends Quadrilaterals {
+    private double length;
+
+    public Square(double length) {
+        super();
+    }
+
+    public Square() {
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    //	A method to find perimeter of a square
+	public void getPerimeter() {
+		System.out.println("The perimeter of a square(: "+this.length+")"
+                                    + " = " + perimeter());	
+	}
+
+    private double perimeter() {
+        return 4 * this.length;
+    }
+
+	//	A method to find area of a square
+	public void getArea() {
+		System.out.println("The area of a square(: "+this.length+")"
+                                    + " = " + area());	
+	}
+
+    private double area() {
+        return this.length * this.length;
+    }
+
 
 }
